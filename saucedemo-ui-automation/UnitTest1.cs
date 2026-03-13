7831ab36e5f934d2f3ab85e393f7e704ae49c1b3
Shashii Gonnagaha <shashii@Shashiis-MacBook-Pro.local>
2026-03-13 16:20:32 +0530

    Initial project creation

---
+++ saucedemo-ui-automation/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace saucedemo_ui_automation;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
